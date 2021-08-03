95a46c02aa1745f1e74032cc918ac8b102771a80
Orlando Arnosa <oarnosa@gmail.com>
2021-08-03 12:53:17 -0400

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+const Home = (): JSX.Element => (
+  <>
+    <h1>Next.js Starter Template</h1>
+    <p>
+      Starter template configured with Next.js, TypeScript, ESLint, Prettier,
+      Git Hooks, and Husky.
+    </p>
+  </>
+);
+
+export default Home;
